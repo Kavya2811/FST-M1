384f4f45d7b0217f51265c6257ef7dc6a71d330c
Kavya2811 <107044085+Kavya2811@users.noreply.github.com>
2022-06-07 18:27:14 +0530

    Added SQL query activities

---
+++ SQL/Activities/Activity1.sql.sql
@@ -0,0 +1,14 @@
+REM   Script: Activity1
+REM   Creating a Table
+
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+DESCRIBE salesman
+
+
